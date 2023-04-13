c694f668bfd90aa6544bafc185bf210c5bc74836
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-13 20:47:50 +0300

    Add Overview

---
+++ iExpense/Overview.swift
@@ -0,0 +1,20 @@
+//
+//  Overview.swift
+//  iExpense
+//
+//  Created by Edmond Podlegaev on 13.04.2023.
+//
+
+import SwiftUI
+
+struct Overview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Overview_Previews: PreviewProvider {
+    static var previews: some View {
+        Overview()
+    }
+}
