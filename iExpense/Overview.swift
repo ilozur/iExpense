38e08ab9ea295b9adf1f0735217983507e17263c
Edmond Podlegaev <50552703+ilozur@users.noreply.github.com>
2023-04-17 23:45:22 +0300

    Update Overview.swift

--- iExpense/Overview.swift
+++ iExpense/Overview.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct Overview: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        }
 }
 
 struct Overview_Previews: PreviewProvider {
