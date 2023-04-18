75b52a8d91fa4683adf026f4ba773810fae01bcd
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-18 13:28:07 +0300

    Showing and hiding views

--- iExpense/Overview.swift
+++ iExpense/Overview.swift
@@ -12,15 +12,38 @@ class User: ObservableObject {
     @Published var lastName = "Baggins"
 }
 
+struct NewOverview: View {
+    @Environment(\.dismiss) var dismiss
+    
+    let name: String
+    
+    var body: some View {
+        VStack {
+            Text("Hello, \(name)")
+            Button("Dismiss") { dismiss() }
+        }
+    }
+}
+
+
 struct Overview: View {
     @StateObject private var user = User()
 
+    @State private var showingSheet = false
+    
     var body: some View {
         VStack {
             Text("Your name is \(user.firstName) \(user.lastName)")
             
             TextField("First Name", text: $user.firstName)
             TextField("Last Name", text: $user.lastName)
+            
+            Button("Show Sheet") {
+                showingSheet.toggle()
+            }
+            .sheet(isPresented: $showingSheet) {
+                NewOverview(name: "@ilozur")
+            }
         }
     }
 }
