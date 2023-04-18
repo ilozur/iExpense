7c82d6c1bb62ca14b250c3c57351d4e5013f66dc
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-18 15:49:53 +0300

    Making changes permanent with UserDefaults

--- iExpense/Expenses.swift
+++ iExpense/Expenses.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let saveItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: saveItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
 }
