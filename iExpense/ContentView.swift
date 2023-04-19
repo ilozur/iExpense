662d8bee671411d94b9890de04c2b96d0f91b024
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-19 16:17:33 +0300

    Add some features

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,6 +7,43 @@
 
 import SwiftUI
 
+struct SmallExpense: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+            .foregroundColor(.green)
+    }
+}
+
+struct MediumExpense: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+            .foregroundColor(.yellow)
+    }
+}
+
+struct LargeExpense: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+            .foregroundColor(.red)
+    }
+}
+
+
+extension View {
+    func smallExpenseStyle() -> some View {
+            modifier(LargeExpense())
+    }
+    func mediumExpenseStyle() -> some View {
+            modifier(LargeExpense())
+    }
+    func largeExpenseStyle() -> some View {
+            modifier(LargeExpense())
+    }
+}
+
 struct ContentView: View {
     @StateObject var expenses = Expenses()
     
@@ -18,12 +55,27 @@ struct ContentView: View {
                 ForEach(expenses.items) { item in
                     HStack {
                         VStack (alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
+                            if item.amount > 100 {
+                                Text(item.name)
+                                    .largeExpenseStyle()
+                            }
+                            if item.amount > 50 && item.amount <= 99 {
+                                Text(item.name)
+                                    .mediumExpenseStyle()
+                            }
+                            if item.amount > 10 && item.amount <= 49 {
+                                Text(item.name)
+                                    .smallExpenseStyle()
+                            }
+                            if item.amount > 1 && item.amount <= 9 {
+                                Text(item.name)
+                                    .font(.headline)
+                            }
                             Text(item.type)
+                                .foregroundColor(.secondary)
                         }
                         Spacer()
-                        Text(item.amount, format: .currency(code: "USD"))
+                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "RUB"))
                     }
                 }
                 .onDelete(perform: removeItems)
